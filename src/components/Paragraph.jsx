72ed452b43a8e205d59c98e0dc390be259e202b5
abhay0480sharma <abhay@openinnovationslab.com>
2023-12-21 22:39:26 +0530

    Add dark theme responsive padding

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -5,7 +5,7 @@ const Paragraph = ({children,className}) => {
   const {theme} = useContext(ThemeContext)
 
   return (
-    <div className={`${!theme?"text-[#427AA1]":"text-[#EBF2FA]"} font-normal text-[18px] my-5 ${className}`}>{children}</div>
+    <div className={`${!theme?"text-[#427AA1]":"text-[#EBF2FA]"} font-normal text-[18px] py-5 ${className}`}>{children}</div>
   )
 }
 
